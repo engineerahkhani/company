4b7a1f77b786d59832f44b40bf42d76b01a7e5c7
AHMAD <engineerahkhani@gmail.com>
2016-02-25 18:44:56 +0330

    some idea

--- index.php
+++ index.php
@@ -1,172 +1,209 @@
 <!DOCTYPE html>
 <html lang="fa">
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.ico">
+
     <link href="css/bootstrap.css" type="text/css" rel="stylesheet">
+    <link href="fonts/iranianSans/iranianSans.css" type="text/css" rel="stylesheet">
+
+    <link href="css/bootstrap-rtl.css" type="text/css" rel="stylesheet">
     <link href="css/styleSheet.css" type="text/css" rel="stylesheet">
     <link href="css/font-awesome.min.css" type="text/css" rel="stylesheet">
     <link href="css/brand-colors.latest.min.css" type="text/css" rel="stylesheet">
     <script src="js/jquery.js"></script>
 
-    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.rtl.min.js"></script>
+
     <title>Company</title>
 </head>
 <body>
-<div class="container-fluid">
+<div class="container">
     <div class="row">
-        <div class="jumbotron" id="landing-section">
-            <div class="container text-center">
-                <h1>فروشگاه کیف و کفش</h1>
-
-                <h2>متناسب با همه سلیقه ها</h2>
-            </div>
-        </div>
-
-        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="420">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Logo</a>
+        <div id="wrapContainer" class="col-md-10 col-md-offset-1">
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Logo</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Projects</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                        </ul>
+                    </div>
                 </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">Products</a></li>
-                        <li><a href="#">Deals</a></li>
-                        <li><a href="#">Stores</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#"><span class=""></span> Your Account</a></li>
-                        <li><a href="#"><span class=" "></span> Cart</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-<!-- end nave-->
+            </nav>
 
-        <div id="carousel">
             <div class="row">
-                <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#myCarousel" data-slide-to="1"></li>
-                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                        <li data-target="#myCarousel" data-slide-to="3"></li>
-                    </ol>
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                            <img src="img/2.jpg" alt="Chania">
+                <div class="col-sm-7">
+                    <div class="jumbotron">
 
-                            <div class="carousel-caption">
-                                <h3>Chania</h3>
-
-                                <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                            </div>
-                        </div>
-
-                        <div class="item">
-                            <img src="img/4.jpg" alt="Chania">
-
-                            <div class="carousel-caption">
-                                <h3>Chania</h3>
-
-                                <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                            </div>
-                        </div>
-
-                        <div class="item">
-                            <img src="img/7.jpg" alt="Flower">
-
-                            <div class="carousel-caption">
-                                <h3>Flowers</h3>
-
-                                <p>Beatiful flowers in Kolymbari, Crete.</p>
-                            </div>
-                        </div>
-
-                        <div class="item">
-                            <img src="img/1.jpg" alt="Flower">
-
-                            <div class="carousel-caption">
-                                <h3>Flowers</h3>
-
-                                <p>Beatiful flowers in Kolymbari, Crete.</p>
-                            </div>
+                        <div class="well text-danger">
+                            <h2>فروشگاه کیف و کفش </h2>
                         </div>
                     </div>
 
-                    <!-- Left and right controls -->
-                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                        <span class="fa fa-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                        <span class="fa fa-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
                 </div>
-            </div>
-        </div>
-        <br>
+                <div class="col-sm-5">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                        </ol>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">BLACK FRIDAY DEAL</div>
-                        <div class="panel-body"><img src="img/1.jpg" class="img-responsive"
-                                                     style="width:100%" alt="Image"></div>
-                        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">BLACK FRIDAY DEAL</div>
-                        <div class="panel-body"><img src="img/2.jpg" class="img-responsive"
-                                                     style="width:100%" alt="Image"></div>
-                        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">BLACK FRIDAY DEAL</div>
-                        <div class="panel-body"><img src="img/4.jpg" class="img-responsive"
-                                                     style="width:100%" alt="Image"></div>
-                        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="http://placehold.it/800x400?text=IMAGE" alt="Image">
+
+                                <div class="carousel-caption">
+                                    <h3>Sell $</h3>
+
+                                    <p>Money Money.</p>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <img src="http://placehold.it/800x400?text=Another Image Maybe" alt="Image">
+
+                                <div class="carousel-caption">
+                                    <h3>More Sell $</h3>
+
+                                    <p>Lorem ipsum...</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                     </div>
                 </div>
             </div>
+            <hr>
+
+            <div class=" text-center">
+                <h3>جدیدترین ها</h3>
+                <br>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                             alt="Image">
+
+                        <p>Current Project</p>
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                             alt="Image">
+
+                        <p>Project 2</p>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="well">
+                            <p>Some text..</p>
+                        </div>
+                        <div class="well">
+                            <p>Some text..</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="well">
+                            <p>Some text..</p>
+                        </div>
+                        <div class="well">
+                            <p>Some text..</p>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            </div>
+
+            <div class=" text-center">
+                <h3>مشتریان ما</h3>
+                <br>
+
+                <div class="row">
+                    <div class="col-sm-2">
+                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                             alt="Image">
+
+                        <p>Partner 1</p>
+                    </div>
+                    <div class="col-sm-2">
+                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                             alt="Image">
+
+                        <p>Partner 2</p>
+                    </div>
+                    <div class="col-sm-2">
+                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                             alt="Image">
+
+                        <p>Partner 3</p>
+                    </div>
+                    <div class="col-sm-2">
+                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                             alt="Image">
+
+                        <p>Partner 4</p>
+                    </div>
+                    <div class="col-sm-2">
+                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                             alt="Image">
+
+                        <p>Partner 5</p>
+                    </div>
+                    <div class="col-sm-2">
+                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                             alt="Image">
+
+                        <p>Partner 6</p>
+                    </div>
+                </div>
+            </div>
+            <br>
+
+            <footer class=" text-center">
+                <p>Footer Text</p>
+            </footer>
+
         </div>
-        <br><br>
-
-        <footer class="container-fluid text-center">
-            <p>Online Store Copyright</p>
-
-            <form class="form-inline">Get deals:
-                <input type="email" class="form-control" size="50" placeholder="Email Address">
-                <button type="button" class="btn btn-danger">Sign Up</button>
-            </form>
-        </footer>
     </div>
 </div>
+
+<!-- jQuery -->
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
 <script>
-    $(document).ready(function () {
-        $('.carousel').carousel({
-            interval: 3000
-        });
-    });
+
 </script>
+
+</body>
+
 </body>
 </html>
