b43030b99433d704528b18a806176584fe639fa6
AHMAD <engineerahkhani@gmail.com>
2016-02-27 20:01:32 +0330

    some improvement made

--- index.php
+++ index.php
@@ -38,10 +38,10 @@
                     </div>
                     <div class="collapse navbar-collapse" id="myNavbar">
                         <ul class="nav navbar-nav">
-                            <li class="active"><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Contact</a></li>
+                            <li class="active"><a href="#">خانه</a></li>
+                            <li><a href="#">مردانه</a></li>
+                            <li><a href="#">زنانه</a></li>
+                            <li><a href="#">بچه گانه</a></li>
                         </ul>
                         <ul class="nav navbar-nav navbar-right">
                         </ul>
@@ -50,6 +50,7 @@
             </nav>
 
             <div class="row">
+                <br>
                 <div class="col-sm-7">
                     <div class="jumbotron">
 
@@ -57,7 +58,6 @@
                             <h2>فروشگاه کیف و کفش </h2>
                         </div>
                     </div>
-
                 </div>
                 <div class="col-sm-5">
                     <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -66,11 +66,10 @@
                             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                             <li data-target="#myCarousel" data-slide-to="1"></li>
                         </ol>
-
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner" role="listbox">
                             <div class="item active">
-                                <img src="http://placehold.it/800x400?text=IMAGE" alt="Image">
+                                <img src="img/1.jpg" alt="Image">
 
                                 <div class="carousel-caption">
                                     <h3>Sell $</h3>
@@ -80,11 +79,10 @@
                             </div>
 
                             <div class="item">
-                                <img src="http://placehold.it/800x400?text=Another Image Maybe" alt="Image">
+                                <img src="img/2.jpg" alt="Image">
 
                                 <div class="carousel-caption">
                                     <h3>More Sell $</h3>
-
                                     <p>Lorem ipsum...</p>
                                 </div>
                             </div>
@@ -107,80 +105,83 @@
             <div class=" text-center">
                 <h3>جدیدترین ها</h3>
                 <br>
+                <!-- Centered Tabs -->
+                <ul class="nav nav-tabs nav-justified">
+                    <li class="active"><a data-toggle="tab" href="#home">مردانه</a></li>
+                    <li><a data-toggle="tab" href="#menu1">زنانه</a></li>
+                    <li><a data-toggle="tab" href="#menu2">بچگانه</a></li>
+                </ul>
 
-                <div class="row">
-                    <div class="col-sm-3">
-                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
-                             alt="Image">
-
-                        <p>Current Project</p>
+                <div class="tab-content">
+                    <div id="home" class="tab-pane fade in active">
+                        <br>
+                        <div class="row">
+                    <div class="col-sm-2">
+                            <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                                 alt="Image">
+                            <p>Partner 1</p>
+                        </div>
+                    <div class="col-sm-2">
+                            <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                                 alt="Image">
+                            <p>Partner 2</p>
+                        </div>
+                    <div class="col-sm-2">
+                            <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                                 alt="Image">
+                            <p>Partner 3</p>
+                        </div>
+                    <div class="col-sm-2">
+                            <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                                 alt="Image">
+                            <p>Partner 4</p>
+                        </div>
+                    <div class="col-sm-2">
+                            <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                                 alt="Image">
+                            <p>Partner 5</p>
+                        </div>
+                    <div class="col-sm-2">
+                            <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
+                                 alt="Image">
+                            <p>Partner 6</p>
+                        </div>
+                        </div>
                     </div>
-                    <div class="col-sm-3">
-                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
-                             alt="Image">
-
-                        <p>Project 2</p>
+                    <div id="menu1" class="tab-pane fade">
+                        <h3>Menu 1</h3>
+                        <p>Some content in menu 1.</p>
                     </div>
-                    <div class="col-sm-3">
-                        <div class="well">
-                            <p>Some text..</p>
-                        </div>
-                        <div class="well">
-                            <p>Some text..</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-3">
-                        <div class="well">
-                            <p>Some text..</p>
-                        </div>
-                        <div class="well">
-                            <p>Some text..</p>
-                        </div>
+                    <div id="menu2" class="tab-pane fade">
+                        <h3>Menu 2</h3>
+                        <p>Some content in menu 2.</p>
                     </div>
                 </div>
                 <hr>
             </div>
 
             <div class=" text-center">
-                <h3>مشتریان ما</h3>
+                <h3>هلی شات</h3>
                 <br>
 
                 <div class="row">
-                    <div class="col-sm-2">
+                    <div class="col-sm-4">
                         <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
                              alt="Image">
 
                         <p>Partner 1</p>
                     </div>
-                    <div class="col-sm-2">
+                    <div class="col-sm-4">
                         <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
                              alt="Image">
 
                         <p>Partner 2</p>
                     </div>
-                    <div class="col-sm-2">
+                    <div class="col-sm-4">
                         <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
                              alt="Image">
 
                         <p>Partner 3</p>
-                    </div>
-                    <div class="col-sm-2">
-                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
-                             alt="Image">
-
-                        <p>Partner 4</p>
-                    </div>
-                    <div class="col-sm-2">
-                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
-                             alt="Image">
-
-                        <p>Partner 5</p>
-                    </div>
-                    <div class="col-sm-2">
-                        <img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
-                             alt="Image">
-
-                        <p>Partner 6</p>
                     </div>
                 </div>
             </div>
